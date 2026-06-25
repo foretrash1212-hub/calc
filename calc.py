e4a34a847ddc549e5371d5590d75634b5573d6e8
Anand <foretrash1212@gmail.com>
2026-06-25 09:11:33 +0530

    Subtraction function created

--- calc.py
+++ calc.py
@@ -1,6 +1,10 @@
 def add():
 	sum = int(input("Enter the first number: ")) + int(input("Enter the second number:"))
 	print("the addition of two numbers is:", sum)
+def sub():
+	diff = int(input("Enter the first number: ")) - int(input("Enter the second number:"))
+	print("the subtraction of two numbers is:", diff)
 
 if __name__ == "__main__":
 	add()
+	sub()
