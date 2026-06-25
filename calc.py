8dd26312ed2ae0bdda7d283574a0ffd205bb19c8
Anand <foretrash1212@gmail.com>
2026-06-25 08:44:14 +0530

    Addition function created

---
+++ calc.py
@@ -0,0 +1,6 @@
+def add():
+	sum = int(input("Enter the first number: ")) + int(input("Enter the second number:"))
+	print("the addition of two numbers is:", sum)
+
+if __name__ == "__main__":
+	add()
