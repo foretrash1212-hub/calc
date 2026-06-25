c9fdf25e3a1100a5b3d662038e562e5c5c1fb8ec
foretrash1212-hub <foretrash1212@gmail.com>
2026-06-25 09:09:43 +0530

    multiplication function created

--- calc.py
+++ calc.py
@@ -2,5 +2,10 @@ def add():
 	sum = int(input("Enter the first number: ")) + int(input("Enter the second number:"))
 	print("the addition of two numbers is:", sum)
 
+def mult():
+	mul = int(input("Enter the first number: ")) * int(input("Enter the second number:"))
+	print("the multiplication of two numbers is:", mul)
+
 if __name__ == "__main__":
 	add()
+	mult()
