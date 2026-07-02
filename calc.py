2252eacedaee0986dd7eceed706e5acdc5529efa
Anand <foretrash1212@gmail.com>
2026-07-02 14:53:49 +0530

    created add function

--- calc.py
+++ calc.py
@@ -1,15 +1,6 @@
 def add():
 	sum = int(input("Enter the first number: ")) + int(input("Enter the second number:"))
 	print("the addition of two numbers is:", sum)
-def sub():
-	diff = int(input("Enter the first number: ")) - int(input("Enter the second number:"))
-	print("the subtraction of two numbers is:", diff)
-
-def mult():
-	mul = int(input("Enter the first number: ")) * int(input("Enter the second number:"))
-	print("the multiplication of two numbers is:", mul)
 
 if __name__ == "__main__":
 	add()
-	sub()
-	mult()
